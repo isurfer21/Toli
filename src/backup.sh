c7582cfdbb25fb632edc8e488f568f02500e4202
Abhishek Kumar <kumarab@hcl.com>
2017-07-13 17:10:20 +0530

    Git support added

--- src/backup.sh
+++ src/backup.sh
@@ -4,19 +4,40 @@ printf "\nBackup (v 0.0.1)\nCopyright (c) Abhishek Kumar. All rights reserved.\n
 printf "\nStarted backup\n"
 
 timestamp=$(date +"%Y%m%dT%H%M%SIST")
-cd "/Users/kumarab/Documents/Backups/mysql/"
+repoBranch="master"
+repoLoc="/Users/kumarab/Documents/Backups/mysql/"
 
-function dbdump() {
-	dbname=$1
-	printf "\nBacking up: $dbname\n"
-	"/usr/local/mysql/bin/mysqldump" -u root -pHCL@kaW8 "$dbname" > "$dbname.sql"
-	printf "Stored at $PWD/$dbname.sql\n"
+function getRecovery() {
+	schema=$1
+	printf "\nBacking up: $schema\n"
+	"/usr/local/mysql/bin/mysqldump" -u root -pHCL@kaW8 "$schema" > "$schema.sql"
+	printf "Stored at $PWD/$schema.sql\n"
 }
 
-dbdump "crat"
-dbdump "uca_crat"
-dbdump "MaterialTracker"
-dbdump "my_wiki"
-dbdump "doc"
+cd "$repoLoc"
+getRecovery "crat"
+getRecovery "uca_crat"
+getRecovery "MaterialTracker"
+getRecovery "my_wiki"
+getRecovery "doc"
+
+printf "\nCheckout `$repoBranch` branch\n"
+git checkout "$repoBranch"
+git fetch
+git pull
+
+printf "\nChanged files in `$repoBranch`\n"
+git status
+
+printf "\nAdding all changed files in next commit\n"
+git add .
+
+printf "\nCommiting all local changes in tracked files\n"
+git commit -am "Taken repository backup"
+
+printf "\nPushing all local changes on remote repository in odc_master branch\n"
+git push origin "$repoBranch"
+
+printf "\nFinished at $timestamp\n"
 
 printf "\nDone!\n"
